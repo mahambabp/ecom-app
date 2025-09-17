c3fb47d16187977c9107a8013c11cda9d3c35307
Bafana Mahamba <66889409+mahambabp@users.noreply.github.com>
2025-09-17 16:15:52 +0200

    init node/express ecom app

--- dockerfile
+++ dockerfile
@@ -1,19 +1,9 @@
 FROM node:20-alpine
-
-#app dirirectory & dependencies
-
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci --omit=dev
-
-#copy source
 COPY . .
-
-#security and runtime
-
 USER node
 EXPOSE 8080
-
 HEALTHCHECK --interval=30s --timeout=3s CMD wget -qO- http://localhost:8080/health || exit 1
-
-CMD ["npm", "start"]
+CMD ["npm","start"]
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/health", (_req, res) => res.status(200).send("OK"));
-app.get("/", (_req, res) => res.send("<h1>My E-commerce Application</h1>"));
+app.get("/", (_req, res) => res.send("<h1>E-commerce App</h1>"));
 
-const port = process.env.PORT || 9000;
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`App listening on ${port}`));
